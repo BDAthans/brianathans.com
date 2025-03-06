81eb9a331e66f11630b899906e58a659fb090d80
Brian Athans <bdathans@yahoo.com>
2025-03-06 15:56:23 -0600

    Fixed BaseURL

--- start-server.cmd
+++ start-server.cmd
@@ -4,7 +4,8 @@ Title Hugo Server Starter
 set port=1314
 set serverIP=192.168.1.71
 
-hugo server -p %port% --bind=%serverIP% --bind=127.0.0.1 --renderToMemory
+hugo server -p %port% --bind=127.0.0.1 --renderToMemory
+REM hugo server -p %port% --bind=%serverIP% --renderToMemory
 REM hugo server -p %port% --renderToMemory
 
 echo Hugo server has exited...
